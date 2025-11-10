c5ea07914bb54a1f460f99e77a9b7298266b3eb5
Dimas Darma <dimasdarma35@gmail.com>
2025-11-10 08:03:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Arvore from './pages/Arvore';
-import { VerifyEmail } from './pages/Arvocloud/dashboard/VerifyEmail';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Arvore />} />
-      <Route path="/verify-email" element={<VerifyEmail />} />
     </Routes>
   );
 }
