d0dbdfb7415fc904b6cfd63af99233c490d2e9cf
Dimas Darma <dimasdarma35@gmail.com>
2025-11-10 08:51:52 +0700

    Update index.tsx

--- src/pages/ArvoAgro/index.tsx
+++ src/pages/ArvoAgro/index.tsx
@@ -1,7 +1,14 @@
-export default function ArvoTech() {
+import { Helmet } from "react-helmet";
+
+export default function ArvoAgro() {
   return (
-    <main className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">ArvoAgro</h1>
-    </main>
+    <>
+      <Helmet>
+        <title>ArvoAgro - Distributor Kelapa Antar Pulau</title>
+      </Helmet>
+      <main className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Coming Soon!</h1>
+      </main>
+    </>
   );
 }
